b95cef32a9ed6859961e39bca402465d7562369b
EtherCluster <web3.nimble@gmail.com>
2022-11-30 09:14:21 +0200

    adding gen

--- ainterest/src/components/ExploreContainer.tsx
+++ ainterest/src/components/ExploreContainer.tsx
@@ -1,3 +1,4 @@
+import { IonButton, IonInput, IonItem, IonLabel, IonList, IonTextarea } from '@ionic/react';
 import './ExploreContainer.css';
 
 interface ContainerProps {
@@ -6,10 +7,17 @@ interface ContainerProps {
 
 const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
   return (
-    <div className="container">
-      <strong>{name}</strong>
-      <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-    </div>
+    <IonList>
+ 
+
+ <IonItem>
+        <IonLabel>Regular textarea</IonLabel>
+        <IonTextarea placeholder="Type something here"></IonTextarea>
+      </IonItem>
+      <IonButton expand="block">Block</IonButton>
+    
+
+  </IonList>
   );
 };
 
